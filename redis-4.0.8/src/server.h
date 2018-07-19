032168d5fead5ed3523dcff3753c3750a923fb03
elem-azar-unis <cs_yqzhang@qq.com>
2018-07-19 14:04:00 +0800

    update .gitignore

--- redis-4.0.8/src/server.h
+++ redis-4.0.8/src/server.h
@@ -589,9 +589,9 @@ typedef struct RedisModuleDigest {
 typedef struct redisObject {
     unsigned type:4;
     unsigned encoding:4;
-    unsigned lru:LRU_BITS; /* LRU time (relative to global lru_clock) or
-                            * LFU data (least significant 8 bits frequency
-                            * and most significant 16 bits access time). */
+    unsigned lru:LRU_BITS;   /* LRU time (relative to global lru_clock) or
+                                * LFU data (least significant 8 bits frequency
+                                * and most significant 16 bits access time). */
     int refcount;
     void *ptr;
 } robj;
