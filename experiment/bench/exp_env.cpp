975f8bc53ab383de07b33bcb08c0bfb6b6eb4ce8
Yuqi Zhang <cs.yqzhang@gmail.com>
2020-04-16 00:29:54 +0800

    update exp_env.cpp

--- experiment/bench/exp_env.cpp
+++ experiment/bench/exp_env.cpp
@@ -3,4 +3,14 @@
 //
 #include "exp_env.h"
 
-const char* exp_setting::type_str[3]= {"speed", "replica", "delay"};
+int exp_setting::delay;
+int exp_setting::delay_low;
+int exp_setting::total_servers;
+int exp_setting::total_ops;
+int exp_setting::op_per_sec;
+
+exp_setting::exp_type exp_setting::type;
+const char *exp_setting::pattern_name;
+int exp_setting::round_num;
+
+const char *exp_setting::type_str[3] = {"speed", "replica", "delay"};
