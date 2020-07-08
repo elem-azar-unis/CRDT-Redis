83a2fa80889ccfb98808d531bab6fe82f68b1dde
Yuqi Zhang <cs.yqzhang@gmail.com>
2020-07-08 12:05:17 +0800

    Update result.py

--- experiment/result/result.py
+++ experiment/result/result.py
@@ -9,9 +9,10 @@ def freq(li):
     return len([x for x in li if x != 0])
 
 
-def read(ztype, server, op, delay, low_delay, directory='.'):
-    d = "{dir}/{t}:{s},{o},({d},{ld})".format(dir=directory,
+def read(ztype, server, op, delay, low_delay, root_dir='.'):
+    d = "{dir}/{t}:{s},{o},({d},{ld})".format(dir=root_dir,
                                               t=ztype, s=server, o=op, d=delay, ld=low_delay)
+    global data_skiped
     ovhd = []
     rmax = []
     for line in open(d + "/s.ovhd"):
@@ -31,8 +32,8 @@ def read(ztype, server, op, delay, low_delay, directory='.'):
 
 def cmp_or(name, directory, server=9, op=10000, delay=50, low_delay=10):
     xlable = 'time: second'
-    ormax, oovhd = read("o", server, op, delay, low_delay, directory=directory)
-    rrmax, rovhd = read("r", server, op, delay, low_delay, directory=directory)
+    ormax, oovhd = read("o", server, op, delay, low_delay, root_dir=directory)
+    rrmax, rovhd = read("r", server, op, delay, low_delay, root_dir=directory)
     lmax = min(len(ormax), len(rrmax))
     lovhd = min(len(oovhd), len(rovhd))
     x1 = [i for i in range(lovhd)]
@@ -108,15 +109,17 @@ def plot_line(o_data, r_data, name, x_lable, y_lable):
 def _cmp_generic(exp_settings, directory, x_paras=None, plot_func=None):
     oms = []
     oos = []
-    for set in exp_settings:
-        m, o = read("o", *set, directory=directory)
+    for setting in exp_settings:
+        setting[-1] = directory
+        m, o = read("o", *setting)
         oms.append(m)
         oos.append(o)
 
     rms = []
     ros = []
-    for set in exp_settings:
-        m, o = read("r", *set, directory=directory)
+    for setting in exp_settings:
+        setting[-1] = directory
+        m, o = read("r", *setting)
         rms.append(m)
         ros.append(o)
 
@@ -128,45 +131,54 @@ def _cmp_generic(exp_settings, directory, x_paras=None, plot_func=None):
     return om_avg, rm_avg, om_count, rm_count, oo_max, ro_max
 
 
-def cmp_generic(x_paras, dirs, exp_settings, plot_func):
-    om_avg = [[] for i in range(len(x_paras))]
-    rm_avg = [[] for i in range(len(x_paras))]
-    om_count = [[] for i in range(len(x_paras))]
-    rm_count = [[] for i in range(len(x_paras))]
-    oo_max = [[] for i in range(len(x_paras))]
-    ro_max = [[] for i in range(len(x_paras))]
+def cmp_generic(x_paras, dirs, exp_settings, plot_func, low, high):
+    om_avg_collect = [[] for i in range(len(x_paras))]
+    rm_avg_collect = [[] for i in range(len(x_paras))]
+    om_count_collect = [[] for i in range(len(x_paras))]
+    rm_count_collect = [[] for i in range(len(x_paras))]
+    oo_max_collect = [[] for i in range(len(x_paras))]
+    ro_max_collect = [[] for i in range(len(x_paras))]
 
-    for dir in dirs:
-        a, b, c, d, e, f = _cmp_generic(exp_settings, dir)
+    for setting in exp_settings:
+        setting.append("")
+    for root_dir in dirs:
+        a, b, c, d, e, f = _cmp_generic(exp_settings, root_dir)
         for i in range(len(x_paras)):
-            om_avg[i].append(a[i])
-            rm_avg[i].append(b[i])
-            om_count[i].append(c[i])
-            rm_count[i].append(d[i])
-            oo_max[i].append(e[i])
-            ro_max[i].append(f[i])
+            om_avg_collect[i].append(a[i])
+            rm_avg_collect[i].append(b[i])
+            om_count_collect[i].append(c[i])
+            rm_count_collect[i].append(d[i])
+            oo_max_collect[i].append(e[i])
+            ro_max_collect[i].append(f[i])
+
+    om_avg = []
+    rm_avg = []
+    om_count = []
+    rm_count = []
+    oo_max = []
+    ro_max = []
     for i in range(len(x_paras)):
-        om_avg[i].sort()
-        rm_avg[i].sort()
-        om_count[i].sort()
-        rm_count[i].sort()
-        oo_max[i].sort()
-        ro_max[i].sort()
-        om_avg[i] = statistics.mean(om_avg[i])
-        rm_avg[i] = statistics.mean(rm_avg[i])
-        om_count[i] = statistics.mean(om_count[i])
-        rm_count[i] = statistics.mean(rm_count[i])
-        oo_max[i] = statistics.mean(oo_max[i])
-        ro_max[i] = statistics.mean(ro_max[i])
+        om_avg.append(statistics.mean(
+            om_avg_collect[i][low:(len(dirs) - high)]))
+        rm_avg.append(statistics.mean(
+            rm_avg_collect[i][low:(len(dirs) - high)]))
+        om_count.append(statistics.mean(
+            om_count_collect[i][low:(len(dirs) - high)]))
+        rm_count.append(statistics.mean(
+            rm_count_collect[i][low:(len(dirs) - high)]))
+        oo_max.append(statistics.mean(
+            oo_max_collect[i][low:(len(dirs) - high)]))
+        ro_max.append(statistics.mean(
+            ro_max_collect[i][low:(len(dirs) - high)]))
     return plot_func(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, x_paras)
 
 
-def cmp_delay(rounds):
+def cmp_delay(rounds, low=0, high=0):
     delays = ["{hd}ms,\n{ld}ms".format(
         hd=20 + x * 40, ld=4 + x * 8) for x in range(10)]
     dirs = ["delay/{}".format(x) for x in range(rounds)]
     exp_settings = [[9, 10000, 20 + x * 40, 4 + x * 8] for x in range(10)]
-    return cmp_generic(delays, dirs, exp_settings, delay_plot)
+    return cmp_generic(delays, dirs, exp_settings, delay_plot, low, high)
 
 
 def delay_plot(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, name):
@@ -197,11 +209,11 @@ def delay_plot(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, name):
     return rtn
 
 
-def cmp_replica(rounds):
+def cmp_replica(rounds, low=0, high=0):
     replicas = [3, 6, 9, 12, 15]
     dirs = ["replica/{}".format(x) for x in range(rounds)]
     exp_settings = [[r, 10000, 50, 10] for r in replicas]
-    return cmp_generic(replicas, dirs, exp_settings, replica_plot)
+    return cmp_generic(replicas, dirs, exp_settings, replica_plot, low, high)
 
 
 def replica_plot(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, name):
@@ -232,11 +244,11 @@ def replica_plot(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, name):
     return rtn
 
 
-def cmp_speed(rounds):
+def cmp_speed(rounds, low=0, high=0):
     speeds = [500 + x * 100 for x in range(96)]
     dirs = ["speed/{}".format(x) for x in range(rounds)]
     exp_settings = [[9, n, 50, 10] for n in speeds]
-    return cmp_generic(speeds, dirs, exp_settings, speed_plot)
+    return cmp_generic(speeds, dirs, exp_settings, speed_plot, low, high)
 
 
 def speed_plot(om_avg, rm_avg, om_count, rm_count, oo_max, ro_max, name):
